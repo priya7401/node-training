874d33793836cd141a6e6982d473be8bf0e8c3bf
Lakshmi Priya S P <lakshmipriya.sridhar@rootquotient.com>
2024-06-10 11:18:18 +0530

    added test case for create attachment end-point

--- src/test/attachments/attachment.test.ts
+++ src/test/attachments/attachment.test.ts
@@ -2,7 +2,7 @@ import { createServer } from '../testUtils';
 import request from 'supertest';
 import { Express } from 'express';
 import { seedAttachmentData } from './seed';
-import { getUploadUrl } from '../../utils/awsConfig';
+import { getDownloadUrl, getUploadUrl } from '../../utils/awsConfig';
 
 const BASE_URL = '/api/v1';
 
@@ -43,6 +43,7 @@ beforeEach(async () => {
 
 jest.mock('../../utils/awsConfig', () => ({
   getUploadUrl: jest.fn(),
+  getDownloadUrl: jest.fn(),
 }));
 
 describe('create new attachments', () => {
@@ -77,6 +78,58 @@ describe('create new attachments', () => {
     expect(getUploadUrl).toHaveBeenCalled();
     expect(response.body).toHaveProperty('s3_url', expectedUrl);
     expect(response.body).toHaveProperty('s3_key');
+    expect(response.body.s3_key).not.toBeNull();
+  }, 20000);
+
+  it('should throw error for invalid payload', async () => {
+    let url: string = BASE_URL + '/attachments/create_attachment';
+
+    let response = await request(app).put(url).set('Authorization', `Bearer ${jwtToken}`);
+
+    console.log(response.text);
+
+    expect(response.status).toBe(400);
+    expect(response.text).toBe('Error validating request body. "file_name" is required. "file_type" is required. "s3_key" is required.');
+  }, 20000);
+
+  it('should create new attachment and return the attachment object', async () => {
+    // get s3_key
+    let url: string = BASE_URL + '/attachments/presigned_url';
+
+    const getPresignedUrlRequestBody = {
+      file_name: 'test_file_1.pdf',
+      file_type: 'application/pdf',
+    };
+
+    let expectedUrl = 'https://hello_world_test/s3_upload';
+
+    (getUploadUrl as jest.Mock).mockResolvedValue(expectedUrl);
+
+    let response = await request(app).post(url).set('Authorization', `Bearer ${jwtToken}`).send(getPresignedUrlRequestBody);
+
+    // create new attachment record
+    url = BASE_URL + '/attachments/create_attachment';
+
+    const createAttachmentRequestBody = {
+      file_name: 'test_file_1.pdf',
+      file_type: 'application/pdf',
+      s3_key: response.body.s3_key,
+    };
+
+    expectedUrl = 'https://hello_world_test/s3_download';
+
+    (getDownloadUrl as jest.Mock).mockResolvedValue(expectedUrl);
+
+    response = await request(app).put(url).set('Authorization', `Bearer ${jwtToken}`).send(createAttachmentRequestBody);
+
+    expect(response.status).toBe(201);
+    expect(getDownloadUrl).toHaveBeenCalled();
+    expect(response.body).toHaveProperty('attachment');
+    expect(response.body.attachment).not.toBeNull();
+    expect(response.body).toHaveProperty('attachment.id');
+    expect(response.body.attachment.id).not.toBeNull();
+    expect(response.body).toHaveProperty('attachment.s3_url', expectedUrl);
+    expect(response.body).toHaveProperty('attachment.s3_key');
     expect(response.body.s3_key).not.toBeNull();
   }, 20000);
 });
